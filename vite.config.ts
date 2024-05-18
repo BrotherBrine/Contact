70d8e0ca0beff3ef3c58661afe2f92dd8e7f1332
BrotherBrine <zacharytstevenson@gmail.com>
2024-05-18 14:28:55 -0500

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import macrosPlugin from 'vite-plugin-babel-macros'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Contact/",
   plugins: [
     react({
       babel:{
